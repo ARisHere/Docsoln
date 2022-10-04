abdfb144153fff18789c475a40d1d6454d808975
ARisHere <mdahad.mma2017@gmail.com>
2022-10-05 00:10:04 +0600

    django and database connected

--- Docsoln/Docsoln/wsgi.py
+++ Docsoln/Docsoln/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for docsoln project.
+WSGI config for Docsoln project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docsoln.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Docsoln.settings')
 
 application = get_wsgi_application()
